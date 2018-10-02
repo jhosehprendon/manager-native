2baa83adee0b7f233f45ca8f6f3fe7468f9b5370
Jhosehp <jhosehp.rendon@gmail.com>
2018-10-02 08:38:15 -0500

    Add navigation on successful login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types'
 
 export const emailChanged = (text) => {
@@ -39,4 +40,6 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS, 
         payload: user 
     });
+
+    Actions.employeeList();
 }
